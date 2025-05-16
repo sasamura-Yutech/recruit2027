d03e182125c718e9ac64d79af0e0725c0c87edf9
h-goto <h-goto@yutech.co.jp>
2025-05-16 11:06:10 +0900

    トップページ：社員の声をスライダーカルーセルに変更

---
+++ 採用ページ（後藤作業中）/js/slider.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function($) {
+            "use strict";
+            //  TESTIMONIALS CAROUSEL HOOK
+            $('#carousel').owlCarousel({
+                loop: true,
+                center: true,
+                items: 3,
+                margin: 0,
+                autoplay: true,
+                dots:true,
+                autoplayTimeout: 8500,
+                smartSpeed: 450,
+                stagePadding: 50,
+                responsive: {
+                  0: {
+                    items: 1
+                  },
+                  768: {
+                    items: 2
+                  },
+                  1170: {
+                    items: 3
+                  }
+                }
+            });
+          });
